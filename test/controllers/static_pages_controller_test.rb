fb64e81722411e382d3513bc770ca863cc74fdbe
takuya matsunaga <takanotume24@gmail.com>
2019-08-07 16:58:35 +0900

    終了

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-   def setup
+  def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
